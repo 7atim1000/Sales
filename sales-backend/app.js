86d70703749075c2074723b474cdc0ededb57844
Hatim <7atim1000@gmail.com>
2025-08-11 00:57:13 +0400

    Update app.js

--- sales-backend/app.js
+++ sales-backend/app.js
@@ -21,7 +21,7 @@ connectCloudinary();
 // cors policy to unblock response
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173']
+    origin: ['https://sales-1-7arh.onrender.com']
  }))
  
 //Middleware Parse incoming request in json format and cookie parser for cookies and token 
